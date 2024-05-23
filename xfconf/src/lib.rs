45e0b2021c2ff1cdc0e7a4d81c46a7ab5a5bafb4
Brian J. Tarricone <brian@tarricone.org>
2024-05-23 02:50:59 -0700

    Make property getter/setter idiomatic
    The various typed property getters/setters in C (e.g.
    xfconf_channel_get_int()) are not idiomatic in Rust; we can have a
    single method on `Channel` that takes a type parameter for the desired
    type, with traits that handle the conversions between the Rust types and
    `glib::Value`.
    
    This ends up being more robust than the C code, as these conversion
    functions only return successfully-converted values if the value can be
    converted to the desired type without loss of precision.
    
    This also fixes up some missing functionality around 16-bit ints.

--- xfconf/src/lib.rs
+++ xfconf/src/lib.rs
@@ -2,10 +2,13 @@
 
 mod auto;
 mod channel;
+mod conv;
 mod functions;
+mod types;
 
 pub use auto::functions::*;
 pub use auto::*;
 pub use channel::*;
 pub use ffi;
 pub use functions::*;
+pub use types::*;
