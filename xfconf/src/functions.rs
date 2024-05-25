2d6e6e5b99030f89b674df67b825bdaa281cf1c5
Brian J. Tarricone <brian@tarricone.org>
2024-05-24 22:07:44 -0700

    Manually write shutdown() docs
    I want to include a 'safety' section, so tell rustdoc-stripper not to
    touch it.

--- xfconf/src/functions.rs
+++ xfconf/src/functions.rs
@@ -1,7 +1,12 @@
+// rustdoc-stripper-ignore-next
 /// Shuts down and frees any resources consumed by the Xfconf library.
+///
 /// If [`init()`][crate::init()] is called multiple times, [`shutdown()`][crate::shutdown()] must be
 /// called an equal number of times to shut down the library.
-#[doc(alias = "xfconf_shutdown")]
+///
+/// # Safety
+///
+/// You must ensure that there are no [crate::Channel] instances that are live
 pub unsafe fn shutdown() {
     ffi::xfconf_shutdown();
 }
